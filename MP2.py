f1b8a45038d2d216aa15672e2a95c3c3118aae85
Hael <omilaralph@gmail.com>
2017-12-12 18:07:38 +0800

    Added subject_relationship stuff funnctions

--- MP2.py
+++ MP2.py
@@ -82,44 +82,68 @@ def search_subject(database):
 		if subn == i:
 			return i
 
+def update_subjects(db_1, db_2, db_3):
+	for subj in db_1:
+		if (subj in db_2) == False:
+			db_2[subj] = {}
+		if (subj in db_3) == False:
+			db_3[subj] = {}
+	return db_2, db_3
+
 # ^ add more security features
 	
-def assign_grade(database):
-	s_no = search_subject(database)
-	database[s_no][raw_input("Student number (ex: 201509321): ")] = float(input("Enter grade: "))
-	return database
-	
 def enlist_student(database):
-	s_no = search_subject(database)
-	database[s_no][raw_input("Student number (ex: 201509321): ")] = "GRADE"
-	print database
+	subj_no = search_subject(database)
+	stud_no = raw_input("Student number (ex: 201509321): ")
+	if stud_no in database[subj_no]:
+		print ("Error! Student already enrolled!")
+		return database
+	database[subj_no][stud_no] = "n/a"
 	return database
 
-def assign_faculty(database):
-	s_no = search_subject(database)
-	database[s_no].append(raw_input("Faculty number (ex: 201509321): "))
-	print database
-	return database
-	
 def remove_student(database):
 	s_no = search_subject(database)
 	student = raw_input("Student number (ex: 201509321): ")
+	if (student in database[s_no]) == False:
+		print "Error! Student not enrolled in class!"
+		return database
+	if database[subj_no][stud_no] != "n/a":
+		print ("Error! Student already has a grade. Student cannot be removed.")
+		return database
 	for i in database[s_no]:
 		if i == student:
 			database[s_no].pop(student, None)
 			break
 	return database
 
+def assign_grade(database):
+	s_no = search_subject(database)
+	database[s_no][raw_input("Student number (ex: 201509321): ")] = float(input("Enter grade: "))
+	return database
+	
+def assign_faculty(database):
+	s_no = search_subject(database)
+	fac_no = raw_input("Faculty number (ex: 201509321): ")
+	if fac_no in database[s_no]:
+		print "Error! Faculty already assigned."
+		return database
+	database[s_no].append(fac_no)
+	print database
+	return database
+	
 def remove_faculty(database):
 	s_no = search_subject(database)
 	faculty = raw_input("Faculty number (ex: 201509321): ")
+	if (faculty in database[s_no]) == False:
+		print "Error! Faculty is not assigned to this subject."
+		return database
 	for i in database[s_no]:
 		if i == faculty:
 			database[s_no].remove(faculty)
 			break
 	return database
 
-def read_file( file, ref ):
+def read_file(file, ref):
 	ctr = 0 
 	ctr2 = 0
 	database = {}
@@ -136,7 +160,7 @@ def read_file( file, ref ):
 		ctr2 = ctr2 + 1
 	return database
 
-def read_file_special(file):
+def read_file_2(file):
 	ctr = 0
 	database = {}
 	for line in file:
@@ -152,7 +176,7 @@ def read_file_special(file):
 		ctr = ctr + 1
 	return database
 
-def read_file_special2(file):
+def read_file_3(file):
 	database = {}
 	for line in file:
 		line = line.split("\n")
@@ -169,7 +193,6 @@ def open_file(fname):
 		print "Error! File not found. Now creating file."
 		file = file = open(str(fname + ".txt"), "w+")
 	return file
-
 			
 def save_file(database, file, ref, filename):
 	file.close()
@@ -183,27 +206,74 @@ def save_file(database, file, ref, filename):
 				file.write(database[key][ref[i]]+'\n')
 	return file
 
-	
+def save_file_2(database, file, filename):
+	file.close()
+	file = open(filename+".txt", "w+")
+	for key in database:
+		file.write(key+':')
+		ctr = (len(database[key]))
+		print ctr
+		for s_no in database[key]:
+			if ctr != 1:
+				file.write(s_no+","+database[key][s_no]+";")
+			else:
+				file.write(s_no+","+database[key][s_no]+"\n")
+			ctr = ctr - 1
+	return file
+
+def save_file_3(database, file, filename):
+	file.close()
+	file = open(filename+".txt", "w+")
+	for key in database:
+		file.write(key+':')
+		for i in range(0, len(database[key])):
+			if (i != len(database[key])-1):
+				file.write(database[key][i]+',')
+			else:
+				file.write(database[key][i]+'\n')
+	return file
+
 #main
-student_ref = {0: "f_name", 1: "l_name", 2: "m_name", 3: "suffix", 4: "sex", 5: "bday", 6: "course"}
-student_file = open_file("student")
-student_database = read_file(student_file, student_ref)
-print student_database
-student_file = save_file(student_database, student_file, student_ref, "student")
+# student_ref = {0: "f_name", 1: "l_name", 2: "m_name", 3: "suffix", 4: "sex", 5: "bday", 6: "course"}
+# student_file = open_file("student")
+# student_database = read_file(student_file, student_ref)
+# print student_database
+# student_file = save_file(student_database, student_file, student_ref, "student")
 
-faculty_ref = {0: "f_name", 1: "l_name", 2: "m_name", 3: "suffix", 4: "sex", 5: "bday", 6: "college", 7: "dept"}
-faculty_file = open_file("faculty")
-faculty_database = read_file(faculty_file, faculty_ref)
-print faculty_database
-faculty_file = save_file(faculty_database, faculty_file, faculty_ref, "faculty")
+# faculty_ref = {0: "f_name", 1: "l_name", 2: "m_name", 3: "suffix", 4: "sex", 5: "bday", 6: "college", 7: "dept"}
+# faculty_file = open_file("faculty")
+# faculty_database = read_file(faculty_file, faculty_ref)
+# print faculty_database
+# faculty_file = save_file(faculty_database, faculty_file, faculty_ref, "faculty")
 
-subject_ref = {0: "code", 1: "title", 2: "e_year", 3: "status"}
-subject_file = open_file("subject")
-subject_database = read_file(subject_file, subject_ref)
-subject_database = edit_subject(subject_database)
-print subject_database
-subject_file = save_file(subject_database, subject_file, subject_ref, "subject")
-# subject_student_ref = {0: "list", 1: "s_number", 2: "grade"}
+# subject_ref = {0: "code", 1: "title", 2: "e_year", 3: "status"}
+# subject_file = open_file("subject")
+# subject_database = read_file(subject_file, subject_ref)
+# subject_database = add_subject(subject_database, subject_ref)
+# subject_student_database, subject_faculty_database = update_subjects(subject_database, subject_student_database, subject_faculty_database)
+# print subject_database
+# subject_file = save_file(subject_database, subject_file, subject_ref, "subject")
+
+# subject_student_file = open_file("subject_student")
+# subject_student_database =  read_file_2(subject_student_file)
+# print subject_student_database
+# subject_student_database = enlist_student(subject_student_database)
+# print subject_student_database
+# subject_faculty_database = remove_student(subject_student_database)
+# print subject_student_database
+# subject_student_file = save_file_2(subject_student_database, subject_student_file, "subject_student")
+
+subject_faculty_file = open_file("subject_faculty")
+subject_faculty_database =  read_file_3(subject_faculty_file)
+print subject_faculty_database
+subject_faculty_database = assign_faculty(subject_faculty_database)
+print subject_faculty_database
+subject_faculty_database = remove_faculty(subject_faculty_database)
+print subject_faculty_database
+subject_faculty_file = save_file_3(subject_faculty_database, subject_faculty_file, "subject_student")
+
+
+
 # subject_student_file = open_file("subject_student")
 # subject_faculty_file = open_file("subject_faculty")
 # subject_student_database = read_file_special(subject_student_file)
